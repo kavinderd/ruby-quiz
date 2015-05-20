0073ea9b7c478f6129927a7e2f2e0deba53af91a
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-05-20 09:38:49 -0700

    working santa assigner, algorithm needs to be improved

--- secret_santa/lib/secret_santa.rb
+++ secret_santa/lib/secret_santa.rb
@@ -1,11 +1,17 @@
-class Person < Struct.new(:first_name, :last_name, :email, :partner); end;
+class Person < Struct.new(:first_name, :last_name, :email, :partner)
+  def full_name
+    first_name + " " + last_name
+  end
+end
 class SecretSanta
   ODD_NUMBER_ERROR = "Not an even number of people"
+  SAME_LAST_NAME_ERROR = "Remaining people have matching last names"
 
-  attr_reader :people
+  attr_reader :people, :assigned_pairs, :errors
   def initialize
     @people = []
-    @assigned_people = []
+    @assigned_pairs = []
+    @errors = []
   end
 
   def add(first_name:, last_name:,email:)
@@ -14,6 +20,32 @@ class SecretSanta
 
   def process!
     return ODD_NUMBER_ERROR unless @people.count % 2 == 0
+    while @people.any?
+      person1 = @people.pop
+      person2 = nil
+      tried_indices = []
+      until person2
+        random_index = (0..(@people.count - 1)).to_a.sample
+        break if tried_indices.count == @people.count
+        if @people[random_index].last_name != person1.last_name
+          person2 = @people.delete_at(random_index)
+        end
+        tried_indices << random_index
+      end
+      if person1 && person2
+        @assigned_pairs << [person1, person2]
+      else
+        @people.push person1
+        @errors << SAME_LAST_NAME_ERROR
+        break
+      end
+    end
+  end
+
+  def print_santas
+    @assigned_pairs.each do |pair|
+      puts "Pair: #{pair.first.full_name} with #{pair.last.full_name}"
+    end
   end
 
 end
--- secret_santa/test/lib/secret_santa.rb
+++ secret_santa/test/lib/secret_santa.rb
@@ -19,4 +19,36 @@ class SecretSantaTest < Minitest::Test
     @santa.add(first_name: "Luke", last_name: "Skywalker", email: "luke@theforce.net")
     assert_equal(SecretSanta::ODD_NUMBER_ERROR, @santa.process!)
   end
+
+  def test_process_people_with_different_last_names
+    @santa.add(first_name: "Luke", last_name: "Skywalker", email: "luke@theforce.net")
+    @santa.add(first_name: "Han", last_name: "Solo", email: "han@nerfherder.com")
+    @santa.add(first_name: "Lando", last_name: "Calrissian", email: "lando@cloudcity.gov")
+    @santa.add(first_name: "Bail", last_name: "Organa", email: "bail@alderran.gov")
+    @santa.process!
+    assert_equal(0, @santa.people.count)
+    assert_equal(2, @santa.assigned_pairs.count)
+    assert_equal(2, @santa.assigned_pairs.first.count)
+  end
+
+  def test_process_people_with_similar_last_name
+    @santa.add(first_name: "Luke", last_name: "Skywalker", email: "luke@theforce.net")
+    @santa.add(first_name: "Han", last_name: "Solo", email: "han@nerfherder.com")
+    @santa.add(first_name: "Lando", last_name: "Calrissian", email: "lando@cloudcity.gov")
+    @santa.add(first_name: "Leia", last_name: "Skywalker", email: "bail@alderran.gov")
+    @santa.process!
+    @santa.assigned_pairs.each do |pair|
+      refute_equal(pair.first.last_name,pair.last.last_name)
+    end
+  end
+
+  def test_process_people_with_only_same_last_names
+    @santa.add(first_name: "Luke", last_name: "Skywalker", email: "luke@theforce.net")
+    @santa.add(first_name: "Leia", last_name: "Skywalker", email: "bail@alderran.gov")
+    @santa.process!
+    assert_equal(0, @santa.assigned_pairs.count)
+    assert_equal(2, @santa.people.count)
+    assert_equal(SecretSanta::SAME_LAST_NAME_ERROR, @santa.errors.first)
+  end
+
 end
